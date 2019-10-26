7cac3e31f3d7a7be8ec12fe82b738a2e0b03e23c
hemingxuan <506626341@qq.com>
2019-10-26 16:32:34 +0800

    项目经验，技能证书的修改删除，预览分享自己的找活名片

---
+++ pages/addcertificate/addcertificate.js
@@ -0,0 +1,116 @@
+// pages/addcertificate/addcertificate.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgArrs: [],
+    idArrs: []
+  },
+  previewImage(e) {
+    console.log(e)
+    let that = this
+    wx.previewImage({
+      urls: that.data.imgArrs,
+      current: e.target.dataset.item,
+    })
+  },
+  chooseImage() {
+    let that = this
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const path = res.tempFilePaths[0]
+        if (that.data.imgArrs.length >= 5) {
+          return
+        }
+        that.data.imgArrs.push(path)
+        // that.data.idArrs.push(id)
+        that.setData({
+          imgArrs: that.data.imgArrs,
+          // idArrs: that.data.idArrs
+        })
+
+        wx.uploadFile({
+          url: '要上传的地址',
+          filePath: path,
+          name: 'image',
+          success: (res) => {
+            console.log(res)
+            // 根据查看结果是否需要json化
+            let { url, id } = JSON.parse(res.data).data
+            console.log(url)
+            console.log(id)
+            that.data.imgArrs.push(url)
+            that.data.idArrs.push(id)
+            that.setData({
+              imgArrs: that.data.imgArrs,
+              idArrs: that.data.idArrs
+            })
+            console.log(that.data.imgArrs)
+            console.log(that.data.idArrs)
+
+          }
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/modify-project-experience/modifyexper.js
@@ -0,0 +1,151 @@
+// pages/modify-project-experience/modifyexper.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgArrs: [],
+    idArrs: [],
+    date:"",
+    regionone: "",
+    showModal: false,
+  },
+  changeReginone(e) {
+    console.log(e.detail.value)
+    this.setData({
+      regionone: e.detail.value[0] + e.detail.value[1] + e.detail.value[2]
+    })
+  },
+
+  preventTouchMove: function () {
+  },
+
+  bindDateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  previewImage(e) {
+    console.log(e)
+    let that = this
+    wx.previewImage({
+      urls: that.data.imgArrs,
+      current: e.target.dataset.item,
+    })
+  },
+  vertify(){
+    console.log(123)
+    this.setData({
+      showModal: false
+    })
+  },
+  obtn() {
+    this.setData({
+      showModal: false
+    })
+  },
+  deleteexper(){
+    this.setData({
+      showModal: true
+    })
+  },
+  chooseImage() {
+    let that = this
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const path = res.tempFilePaths[0]
+        if (that.data.imgArrs.length >= 5) {
+          return
+        }
+        that.data.imgArrs.push(path)
+        // that.data.idArrs.push(id)
+        that.setData({
+          imgArrs: that.data.imgArrs,
+          // idArrs: that.data.idArrs
+        })
+
+        wx.uploadFile({
+          url: '要上传的地址',
+          filePath: path,
+          name: 'image',
+          success: (res) => {
+            console.log(res)
+            // 根据查看结果是否需要json化
+            let { url, id } = JSON.parse(res.data).data
+            console.log(url)
+            console.log(id)
+            that.data.imgArrs.push(url)
+            that.data.idArrs.push(id)
+            that.setData({
+              imgArrs: that.data.imgArrs,
+              idArrs: that.data.idArrs
+            })
+            console.log(that.data.imgArrs)
+            console.log(that.data.idArrs)
+
+          }
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
