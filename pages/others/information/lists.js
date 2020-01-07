0193aa1b625969a099d825f29e0e9d5dfc9a3de6
lcmxkg <744095990@qq.com>
2020-01-07 20:27:02 +0800

    添加footer未读

--- pages/others/information/lists.js
+++ pages/others/information/lists.js
@@ -61,6 +61,7 @@ Page({
       publishActive: footerjs.publishActive,
       showPublishBox: footerjs.showPublishBox
     })
+    footerjs.initMsgNum(this);
   },
   doPublishAction: function() {
     footerjs.doPublishAction(this);
