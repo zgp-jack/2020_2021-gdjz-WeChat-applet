135465662266018df5518d6c591f170788b05bb5
yangchao <1206662047@qq.com>
2020-08-18 20:01:08 +0800

    工种选择

--- pages/issue/index/index.js
+++ pages/issue/index/index.js
@@ -603,8 +603,14 @@ Page({
     let classifyids = this.data.classifies
     //所有工种数组长度
     let len = classifyids.length
-    //如果既没有选择工种也没有匹配工种那么就不需要循环统计匹配工种num
-    if (!ruleLen) return
+    //如果既没有选择工种也没有匹配工种那么就将num置为0
+    if (!ruleLen) {
+      classifyids.forEach(function(item){
+        if (item.num) {
+          item.num = 0
+        }
+      })
+    }
     //记录选择或者详情匹配工种的数量
     for (let i = 0; i < len; i++) {
       let data = classifyids[i].children
@@ -614,9 +620,9 @@ Page({
         if(has !== -1){
           inum++
           //let num = classifyids[i].num || 0
-          // classifyids[i].num = inum
+          classifyids[i].num = inum
         }
-        classifyids[i].num = inum
+        // classifyids[i].num = inum
       }
     }
     this.setData({
