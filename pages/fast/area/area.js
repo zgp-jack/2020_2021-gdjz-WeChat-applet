6f9596f8f102e5e0ba4b1e222a6919ac654fafe8
yangchao <1206662047@qq.com>
2020-09-15 18:46:51 +0800

    快速发布

--- pages/fast/area/area.js
+++ pages/fast/area/area.js
@@ -35,7 +35,33 @@ Page({
     // 工种选择文本
     showClassifyText:"",
     imglen:0,
-    maxNum:3
+    maxNum:3,
+    // 最大图片数量
+    maxImgNum: 0,
+    // 最大工种数量
+    maxWorkNum: 0,
+    // 不匹配库
+    notMateData: [],
+    // 匹配库
+    mateData: [],
+    // 工种字段
+    classifies: [],
+    // 选择一级工种index
+    pindex: 0,
+    // 子类工种数据
+    childClassifies:[],
+    // 根据详情匹配的工种数据
+    rulesClassifyids: [],
+    // 用户选择的工种数据
+    userClassifyids: [],
+    // 所需工种显示的工种文本信息
+    showClassifyText: "",
+    // 选择或者匹配的工种id数组
+    selectedClassifies: [],
+    rrulesClassifyids: [], // 备份
+    ruserClassifyids: [], // 备份
+    rchildClassifies: [], // 备份
+    rclassifies: [],
   },
   mini_user: function(session_key){
     let { token,areaId } = this.data;
@@ -334,7 +360,6 @@ Page({
         amapFun.getRegeo({
           location: userLocation,
           success: function (data) {
-            console.log("positiondata",data)
             _this.setData({
               "addressData.title":data[0].regeocodeData.addressComponent.neighborhood.name,
               "addressData.location":userLocation,
@@ -515,15 +540,13 @@ Page({
   },
   // 点击所需工种显示工种选择
   showWorkTypePicker: function () {
-    //用户在点击一次工种选择框后，便不再自动匹配详情内容
-    app.globalData.isRuleClass = true
     // 避免用户选择之后取消，所以对数据进行一次备份
     this.setData({
-      // rchildClassifies: JSON.parse(JSON.stringify(this.data.childClassifies)),
-      // rrulesClassifyids: JSON.parse(JSON.stringify(this.data.rulesClassifyids)),
-      // ruserClassifyids: JSON.parse(JSON.stringify(this.data.userClassifyids)),
-      // rclassifies: JSON.parse(JSON.stringify(this.data.classifies)),
-      // rpindex: this.data.pindex,
+      rchildClassifies: JSON.parse(JSON.stringify(this.data.childClassifies)),
+      rrulesClassifyids: JSON.parse(JSON.stringify(this.data.rulesClassifyids)),
+      ruserClassifyids: JSON.parse(JSON.stringify(this.data.userClassifyids)),
+      rclassifies: JSON.parse(JSON.stringify(this.data.classifies)),
+      rpindex: this.data.pindex,
       showPicker: true,
     })
   },
@@ -531,20 +554,334 @@ Page({
   cancelWorkTypePicker: function () {
     this.setData({
       showPicker: false,
-      // pindex: this.data.rpindex,
-      // userClassifyids: JSON.parse(JSON.stringify(this.data.ruserClassifyids)),
-      // rulesClassifyids: JSON.parse(JSON.stringify(this.data.rrulesClassifyids)),
-      // childClassifies: JSON.parse(JSON.stringify(this.data.rchildClassifies)),
-      // classifies: JSON.parse(JSON.stringify(this.data.rclassifies)),
+      pindex: this.data.rpindex,
+      userClassifyids: JSON.parse(JSON.stringify(this.data.ruserClassifyids)),
+      rulesClassifyids: JSON.parse(JSON.stringify(this.data.rrulesClassifyids)),
+      childClassifies: JSON.parse(JSON.stringify(this.data.rchildClassifies)),
+      classifies: JSON.parse(JSON.stringify(this.data.rclassifies)),
     })
-    // this.getWorkText()
+    this.getWorkText()
   },
   // 点击工种选择确定按钮
   sureWorkTypePicker: function () {
     this.setData({
       showPicker: false
     })
-    // this.getWorkText()
+    this.getWorkText()
+  },
+  // 初始化获取工种数据
+  initWorkType: function () {
+    return new Promise((resolve, reject) => {
+      // 获取缓存用户信息
+      let u = wx.getStorageSync('userInfo')
+      this.setData({
+        userInfo: u ? u : false
+      })
+      // 发送请求数据
+      let postData = {
+        ...u,
+        type: 'job'
+      }
+      // 获取匹配库信息请求
+      app.appRequestAction({
+        url: "publish/new-mate-job/",
+        way: 'POST',
+        mask: true,
+        params: postData,
+        success: function (res) {
+          let mydata = res.data
+          if (mydata.errcode === "ok") {
+            resolve(mydata)
+          }else{
+            reject(mydata)
+          }
+        }
+      })
+    })
+  },
+  // 初始化加载工种数据
+  initWorkTypeData: function () {
+    this.initWorkType().then(resolve => {
+      // 最大图片数量
+      let maxImgNum = resolve.typeTextArr.maxImageCount;
+      // 最大工种数量
+      let maxWorkNum = resolve.typeTextArr.maxClassifyCount;
+      // 不匹配库
+      let notMateData = resolve.not_mate_data;
+      // 匹配库
+      let mateData = resolve.mate_data;
+      // 工种字段
+      let classifies = resolve.classifyTree;
+      // 返回的数据对象
+      let mateDatas = {
+        maxImgNum,
+        maxWorkNum,
+        notMateData,
+        mateData,
+        classifies
+      }
+      // 保存到data中
+      this.setData({  maxImgNum ,maxWorkNum ,notMateData ,mateData ,classifies,})
+      return mateDatas
+    },reason => {
+      wx.showModal({
+        title: '提示',
+        content: reason.errmsg,
+        showCancel: false,
+        success: function () {
+          let pages = getCurrentPages()
+          let prePage = pages[pages.length - 2]
+          if (prePage) {
+            wx.navigateBack()
+          } else {
+            wx.reLaunch({
+              url: '/pages/index/index',
+            })
+          }
+        }
+      })
+    }).then(resolve => {
+      this.mateClassifyIdsFun(resolve)
+    })
+  },
+  // 初始化子类工种信息和选中状态
+  initChildWorkType: function () {
+    // 父级工种index
+    let index = this.data.pindex;
+    // 匹配工种字段
+    let rids = this.data.rulesClassifyids;
+    // 用户选择工种字段
+    let uids = this.data.userClassifyids;
+    // 获取父级工种对应的子类工种信息
+    let data = JSON.parse(JSON.stringify(this.data.classifies[index].children))
+    // 循环遍历子类工种数据与自动匹配和选择工种数据进行对比相同就将对应工种数据变成选中状态
+    for (let i = 0; i < data.length; i++) {
+      if (rids.findIndex(item => item.id == data[i].id) !== -1) {
+        data[i].checked = true
+      } else {
+        if (uids.findIndex(item => item.id == data[i].id) !== -1) {
+          data[i].checked = true
+        } else {
+          data[i].checked = false
+        }
+      }
+    }
+    // 设置父类对应子类工种数据
+    this.setData({
+      childClassifies: data
+    })
+  },
+  // 选择一级工种信息
+  userCheckPindex: function (e) {
+    // 一级工种index
+    let index = parseInt(e.currentTarget.dataset.index)
+    this.setData({
+      pindex: index
+    })
+    // 初始化子类工种信息
+    this.initChildWorkType()
+  },
+  // 用户选择工种
+  userCheckWorkType: function (e) {
+    // 获取最大工种数量
+    let num = this.data.maxWorkNum
+    // 选择工种id
+    let id = e.currentTarget.dataset.id
+    // 工种选择状态
+    let checked = e.currentTarget.dataset.checked
+    // 工种名称
+    let name = e.currentTarget.dataset.name
+    // 已经匹配的工种数据
+    let rulesClassifyids = this.data.rulesClassifyids
+    // 已经选择的工种数据
+    let userClassifyids = this.data.userClassifyids
+    // 全部工种数据
+    let data = JSON.parse(JSON.stringify(this.data.classifies))
+    // 选择的父类工种index
+    let pindex = this.data.pindex
+    // 如果是选中状态
+    if (checked) {
+      // 匹配的工种找到对应的工种然后删除并将对应的选中数量减1
+      let ri = rulesClassifyids.findIndex(item => item.id == id)
+      if (ri !== -1) {
+        rulesClassifyids.splice(ri, 1)
+        data[pindex].num = data[pindex].num - 1
+      } else {
+       // 选择的工种找到对应的工种然后删除并将对应的选中数量减1
+        let ui = userClassifyids.findIndex(item => item.id == id)
+        userClassifyids.splice(ui, 1)
+        data[pindex].num = data[pindex].num - 1
+      }
+      // 重新设置选择工种数据
+      this.setData({
+        classifies: data
+      })
+    } else {
+      // 从未选中状态到选中状态
+      // 检查选择工种数量
+      let len = rulesClassifyids.length + userClassifyids.length
+      // 大于规定数量给出提示
+      if (len >= num) {
+        app.showMyTips('工种最多可以选择' + num + '个')
+        return false
+      }
+      // 没有超过要求数量添加到用户选择工种数组中
+      userClassifyids.push({
+        id: id,
+        name: name
+      })
+      // 找到选择工种对应的一级工种的num加1
+      let cnum = data[pindex].num || 0
+      data[pindex].num = cnum + 1
+      // 重新设置选择的工种数据和所有工种数据
+      this.setData({
+        userClassifyids: userClassifyids,
+        classifies: data
+      })
+    }
+    // 将选择的工种数据与匹配的工种数据存入缓存
+    this.setEnterInfo('userClassifyids', userClassifyids)
+    this.setEnterInfo('rulesClassifyids', rulesClassifyids)
+    // 初始化子类工种的数据与选中状态
+    this.initChildWorkType()
+  },
+  // 根据详情匹配工种
+  mateClassifyIdsFun: function (resolve) {
+    wx.showLoading({
+      title: '匹配中',
+      icon: 'none',
+      mask: true
+    })
+    //用户根据所需工作自行选择工种
+    let uids = JSON.parse(JSON.stringify(this.data.userClassifyids))
+    //获取招工详情的内容
+    let content = wx.getStorageSync('jiSuData').detail
+    //所需工种最大选择数
+    let maxWorkNum = resolve.maxWorkNum
+    //不匹配的数据
+    let notRules = resolve.notMateData;
+    //不匹配数据长度
+    let notLen = notRules.length;
+    //获取data中匹配数据
+    let needRules = resolve.mateData;
+    //匹配数据长度
+    let needLen = needRules.length;
+    // 不需要的数据
+    let notArr = [];
+    // 需要的数据
+    let needArr = [];
+    // 如果没有详情内容直接返回
+    if (!content) {
+      wx.hideLoading()
+      return false;
+    }
+    // 不需要匹配的关键词
+    for (let i = 0; i < notLen; i++) {
+      if (content.indexOf(notRules[i].keywords) !== -1) {
+        let id = notRules[i].occupation_id;
+        if (notArr.findIndex(item => item.id == id) == -1) {
+          notArr.push({
+            id: id,
+            name: notRules[i].name
+          })
+        }
+      }
+    }
+    // 匹配关键词并且该关键词没有匹配过放入匹配数组中
+    for (let i = 0; i < needLen; i++) {
+      if (content.indexOf(needRules[i].keywords) !== -1) {
+        let id = needRules[i].occupation_id;
+        if (needArr.findIndex(item => item.id == id) == -1) {
+          needArr.push({
+            id: id,
+            name: needRules[i].name
+          })
+        }
+      }
+    }
+    // 过滤不匹配关键词将不匹配的关键词从匹配到的关键词删除
+    for (let i = 0; i < notArr.length; i++) {
+      let id = notArr[i].id;
+      let index = needArr.findIndex(item => item.id == id)
+      if (index !== -1) {
+        needArr.splice(index, 1)
+        }
+      }
+    // 如果用户选择的工种等于规定最大长度将匹配的数据置空
+    // 否则将匹配的数据长度等于总长度减去用户选择的长度
+    let uidsLen = uids.length
+    if(uidsLen >= maxWorkNum){
+      this.setData({
+        rulesClassifyids: []
+      })
+    }else{
+      let needLen = maxWorkNum - uidsLen
+      needArr.splice(needLen)
+      this.setData({
+        rulesClassifyids: needArr
+      })  
+      this.setEnterInfo('rulesClassifyids',needArr)
+    }
+    this.countWorkNum()
+    this.initChildWorkType()
+    this.getWorkText()
+    wx.hideLoading()
+  },
+  // 匹配的工种数量
+  countWorkNum: function () {
+    //根据详情匹配工种字段
+    let rulesClassifyids = JSON.parse(JSON.stringify(this.data.rulesClassifyids))
+    //用户选择工种字段
+    let userClassifyids = JSON.parse(JSON.stringify(this.data.userClassifyids))
+    //匹配工种字段与用户选择工种字段组成一个数组
+    rulesClassifyids = [...rulesClassifyids, ...userClassifyids]
+    //返回所有工种字段id数组
+    rulesClassifyids = rulesClassifyids.map(item => item.id)
+    //rulesClassifyids数组长度
+    let ruleLen = rulesClassifyids.length
+    let classifyids = this.data.classifies
+    //所有工种数组长度
+    let len = classifyids.length
+    //如果既没有选择工种也没有匹配工种那么就将num置为0
+    if (!ruleLen) {
+      classifyids.forEach(function(item){
+        if (item.num) {
+          item.num = 0
+        }
+      })
+    }
+    //记录选择或者详情匹配工种的数量
+    for (let i = 0; i < len; i++) {
+      let data = classifyids[i].children
+      let inum = 0
+      for(let j = 0;j<data.length;j++){
+        let has = rulesClassifyids.indexOf(data[j].id)
+        if(has !== -1){
+          inum++
+        }
+        classifyids[i].num = inum
+      }
+    }
+    this.setData({
+      classifies: classifyids
+    })
+  },
+  // 所需工种显示工种文本信息
+  getWorkText: function () {
+    let list = this.data.userClassifyids.concat(this.data.rulesClassifyids)
+    //记录选中工种的id并存入this.data.selectedClassifies中
+    let selectWorkType = list.map(function (item,index) {
+      return item.id
+    })
+    // 获取数组的前5个
+    list.splice(5)
+    // 获取工种名称数组
+    list = list.map(item => item.name)
+    this.setData({
+      // 拼接工种字符串与
+      showClassifyText: list.join(','),
+      selectedClassifies: selectWorkType
+    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -555,6 +892,9 @@ Page({
     })
     //初始化用户位置信息
     this.initArea()
+    this.initWorkTypeData()
+    // this.mateClassifyIdsFun()
+
   },
 
   /**
